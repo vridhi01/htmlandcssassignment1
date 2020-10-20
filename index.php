a7278db2193a0678b19b6934beb40642a730e612
deepak <deepak@excellencetechnologies.in>
2020-10-20 17:33:23 +0530

    html/css

--- index.php
+++ index.php
@@ -1,33 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>Assignment1</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="index.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-</head>
-<body>
-
-<div class="headerdiv">
-<nav class="navbar">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <img src="logo.png" class="styleimg"/>
-    </div>
-    <ul class="nav navbar-nav rightlink">
-      <li ><a href="#" class="loginstyle">Login to My Account</a></li></br>
-      <input type="text" placeholder="Search entire store here..."> 
-      <img src="search.png" />
-    </ul>
-  </div>
-</nav></br>
-<div  class="line"></div>
-</div>
-  
-
-
-</body>
+   <head>
+      <title>Assignment1</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="index.css">
+   </head>
+   <body>
+      <div class="headerdiv">
+         <div class="div1">
+            <img src="logo.png" class="styleimg"/>
+         </div>
+         <div class="div2">
+            <li ><a href="#" class="loginstyle">Login to My Account</a></li>
+            <div class="innerdiv1">
+               <input type="text" placeholder="Search entire store here..." class="styleinput"> 
+            </div>
+            <div class="innerdiv2">
+               <img src="search.png" class="stylesearch"/>
+            </div>
+         </div>
+         <div class="line"> </div>
+       </div>
+      
+    
+   </body>
 </html>
