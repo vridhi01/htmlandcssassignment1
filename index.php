7a25587ca59ce12cf475c15232c09b51208d978e
deepak <deepak@excellencetechnologies.in>
2020-10-20 15:56:54 +0530

    assignment1

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Assignment1</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="index.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="headerdiv">
+<nav class="navbar">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <img src="logo.png" class="styleimg"/>
+    </div>
+    <ul class="nav navbar-nav rightlink">
+      <li ><a href="#" class="loginstyle">Login to My Account</a></li></br>
+      <input type="text" placeholder="Search entire store here..."> 
+      <img src="search.png" />
+    </ul>
+  </div>
+</nav></br>
+<div  class="line"></div>
+</div>
+  
+
+
+</body>
+</html>
